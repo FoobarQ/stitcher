eea4ee8cb41fd203c79d8fdcca34f206dc0038dc
Foobar Q <joshua.hall2020@outlook.com>
2022-01-22 23:08:06 +0000

    added feature to let user choose what images to bundle

--- main.py
+++ main.py
@@ -1,24 +1,61 @@
 import cv2
 import os
+import sys
+import datetime
+from pick import pick
+
+
+def create_vid(name, out_dir, frames):
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+
+    height, width, _ = cv2.imread(frames[0]).shape
+    fps: int = 15
+
+    video = cv2.VideoWriter(os.path.join(out_dir, name), 0, fps, (width, height))
+    for frame in frames:
+        video.write(cv2.imread(frame))
+    cv2.destroyAllWindows()
+    video.release()
+    print("video: " + name + " created.")
 
 
 def main() -> None:
     folder: str = "test"
-    frames: list[str] = [file for file in os.listdir(folder) if file.find(".jpg") > 0]
-    video_name: str = "test.avi"
+    frames: list[str] = [os.path.join(folder, file) for file in os.listdir(folder) if file.find(".jpg") > 0]
+    interval: int = 10 + 1
+    frame_lists = {}
+    video_name = "test.avi"
     output_folder: str = "output"
-    if not os.path.isdir(os.path.join(folder, output_folder)):
-        os.makedirs(os.path.join(folder, output_folder))
+    previous_time = os.stat(frames[0]).st_ctime
+    video_key = str(datetime.datetime.fromtimestamp(previous_time))
+    frame_lists[video_key] = []
 
-    height, width, _ = cv2.imread(os.path.join(folder, frames[0])).shape
-    fps: int = 30
+    for i in range(len(frames)):
+        current_time = os.stat(frames[i]).st_ctime
 
-    video = cv2.VideoWriter(os.path.join(folder, output_folder, video_name), 0, fps, (width, height))
-    for frame in frames:
-        if frame.find(".jpg"):
-            video.write(cv2.imread(os.path.join(folder, frame)))
-    cv2.destroyAllWindows()
-    video.release()
+        if current_time - previous_time > interval:
+            video_key = str(datetime.datetime.fromtimestamp(current_time))
+            frame_lists[video_key] = []
+
+        frame_lists[video_key].append(frames[i])
+        previous_time = current_time
+    title = "--------------------------------------------------------------\nstart\t\t\t\tduration\t\tframes\n--------------------------------------------------------------\n"
+    options = []
+    for key in frame_lists.keys():
+        if len(frame_lists[key]) - 1:
+            options.append(
+                key
+                + "\t"
+                + str(
+                    datetime.datetime.fromtimestamp(os.stat(frame_lists[key][-1]).st_ctime)
+                    - datetime.datetime.fromtimestamp(os.stat(frame_lists[key][0]).st_ctime)
+                )
+                + "\t\t"
+                + str(len(frame_lists[key]))
+            )
+    chosen_list = frame_lists[pick(options, title)[0].split("\t")[0]]
+    create_vid(video_name, os.path.join(folder, output_folder), chosen_list)
 
 
 if __name__ == "__main__":
